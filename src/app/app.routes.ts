34a3f0071a05c1c965db1df101739e26247d883c
eduardo-weise <eduardo-weise@users.noreply.github.com>
2026-02-27 14:12:18 +0000

    deploy: b1858f257013f6ef7ac0a0adb700efcdb25dd81f

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  { path: '', loadComponent: () => import('./features/presenter-picker/components/presenter-page/presenter-page.component').then(m => m.PresenterPageComponent) },
-  { path: '**', redirectTo: '' }
-];
