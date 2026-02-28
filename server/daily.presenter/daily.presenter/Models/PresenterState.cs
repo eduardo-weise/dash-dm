d79e1dfe79efe1f4565fd8f08d2962964a69e9bd
Eduardo Weise <eduardo.weise@ndd.tech>
2026-02-28 11:32:39 -0300

    refactore: revertendo reestruturação

--- server/daily.presenter/daily.presenter/Models/PresenterState.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace daily.presenter.Models;
-
-public class PresenterState
-{
-    public List<string> Names { get; set; } = [];
-    public int CurrentIndex { get; set; } = 0;
-    public string LastAdvancedDate { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow).ToString("yyyy-MM-dd");
-}
